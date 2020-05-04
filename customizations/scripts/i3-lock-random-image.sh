c6801b1c2991e26a3f9055d9a68a6cb3a82524ec
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-05-04 13:34:10 -0300

    More improvements to wallpapers and lock screens and config organization

---
+++ customizations/scripts/i3-lock-random-image.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+DIR_PATH=/mnt/24847D5F847D3500/Daniel/Wallpapers/
+IMAGE_PATH=$(find $DIR_PATH -name '*.png' -o -name '*.jpg' | shuf -n 1)
+# echo "$IMAGE_PATH"
+convert $IMAGE_PATH -resize 1920x1080 -gravity center -background black -extent 1920x1080 RGB:- | i3lock -t --raw 1920x1080:rgb --image=/dev/stdin
