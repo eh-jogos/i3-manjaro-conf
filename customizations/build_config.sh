2834fceb11e1db95a0e541aac1fd1383c2553a3e
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-09-06 08:44:16 -0300

    Small fixes and updates to various scripts

--- customizations/build_config.sh
+++ customizations/build_config.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 cat ~/.i3/config.bk ~/.i3/customizations/my_manjaro_custom_configs.txt > ~/.i3/config 
+sleep 1
--- window_layouts_by_project/GamejoltApiWindowSetup.sh
+++ window_layouts_by_project/GamejoltApiWindowSetup.sh
@@ -111,10 +111,10 @@ if [ $DUAL_MONITORS = "true" ]
 then
     i3-msg move workspace to output $MONITOR_LEFT
 fi
-firefox --new-window "https://pomodoro-tracker.com/"
+firefox --new-window "https://docs.google.com/spreadsheets/"
 sleep 3
+firefox --new-tab "https://pomodoro-tracker.com/"
 i3-msg layout stacking
-firefox --new-tab "https://docs.google.com/spreadsheets/"
 sleep 1
 firefox --new-tab "https://app.hacknplan.com/"
 sleep 1
